5ef1e0d68d2608fae21e34cf28aade7612e603d0
charlene <charlene0843@gmail.com>
2024-01-05 17:39:22 +0800

    [feat] revX （靜態）

--- src/page/Home.js
+++ src/page/Home.js
@@ -20,13 +20,63 @@ const Home = () => {
                     <button>聯絡我們</button>
                 </div>
             </nav>
+
+
             <div className='banner'>
-                <p className="banner-logo">Shining Text Animation Effects</p>
+                <p className="banner-typing">擁有全方位 Web 3 盡在</p>
                 <ReactSVG
                     className="banner-logo"
                     src={process.env.PUBLIC_URL + '/img/logo/apam_logo_L.svg'} />
+                <p className="banner-logo">Shining Text Animation Effects</p>
+
+                <div className="onoff">
+                    <span className="circle"></span>
+                </div>
+                <div className="social-media">
+                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/fb.svg'} />
+                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/instagram.svg'} />
+                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/line.svg'} />
+                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/twitter.svg'} />
+                </div>
+            </div>
+
+            <div className="rev-x">
+                {/* <ReactSVG src={process.env.PUBLIC_URL + '/img/'}/> */}
+                <img className="rev-x-logo" src={process.env.PUBLIC_URL + '/img/logo/REVX-logo-colored.png'} alt="revX logo" />
+
+                <h2>區塊鏈技術X產業特定需求</h2>
+                <p>在Web3的時代裡，每一次的互動都是永恆的記憶。
+                    <br />
+                    RevX 將區塊鏈技術與主流  IT 服務結合，
+                    <br />
+                    從多種產業別中提取並整合其特定需求，
+                    <br />
+                    打造一個全面且實用的生活化平台。</p>
+
+                <div className="more">
+                    <span>MORE</span>
+                    <ReactSVG src={process.env.PUBLIC_URL + '/img/arrow.svg'} />
+                </div>
+
+                <img className="phone1" src={process.env.PUBLIC_URL + '/img/phone-1.png'} alt="revX logo" />
+                <img className="phone2" src={process.env.PUBLIC_URL + '/img/phone-2.png'} alt="revX logo" />
+
 
             </div>
+
+            <div className="what-we-do">
+                
+            </div>
+
+
+
+
+            <p className="copyright">
+                Copyright © 2022 AP . APAM. All Rights Reserved.
+                <br />
+                A member of the APAM . APAM International Co. Ltd
+            </p>
+
 
         </div>
 

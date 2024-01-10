0847e3a6d61a91e4f2a4eeedf60ccf0989b5235a
charlene <charlene0843@gmail.com>
2024-01-10 13:57:33 +0800

    [feat] introduction [feat] slider

---
+++ src/components/Slider/Slider.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import { ReactSVG } from "react-svg";
+
+
+const Slider = () => {
+    const [current, setCurrent] = useState(0);
+
+    const history = [
+        {
+            id: 0,
+            date: '2021/01',
+            title: '建造第一礦廠',
+            content: '行動勝過言辭，毅力助力成功，逆境培養紀律，禮儀雕塑品格。不僅僅是品牌，更是一種充滿活力的精神——對堅定承諾和持久影響的明證。感受本質，擁抱旅程。',
+
+        },
+        {
+            id: 1,
+            date: '2021/03',
+            title: '擴增礦機設備',
+            content: '挹注領先效能與技術，大幅提升礦機效益'
+        },
+        {
+            id: 2,
+            date: '2021/07',
+            title: '第二礦廠-100坪大型運算中心',
+            content: '打造大型運算中心，強化風險管控安全'
+        },
+        {
+            id: 3,
+            date: '2021/08',
+            title: '提供市場唯一算力出租商品',
+            content: '礦場算力出租服務，佈局現在放眼未來'
+        },
+        {
+            id: 4,
+            date: '2022/03',
+            title: '​建置 Throne of Dawn',
+            content: '奇幻冒險史詩風格，搭配細膩唯美的視覺呈現'
+        },
+        {
+            id: 5,
+            date: '2022/08',
+            title: '新增軟體開發部門',
+            content: '研發新型技術及會員卡相關應用中，秉持著在堅實的技術基礎上創造實用性'
+        },
+        {
+            id: 6,
+            date: '2022/10',
+            title: '獨創TOD去中心化平台上線',
+            content: '​結合新時代的技術、藝術與電商生活，打造適於協作、廣泛交互傳播的平台'
+        },
+        {
+            id: 7,
+            date: '2023/04',
+            title: 'DAPP RevXBeta 系統上線',
+            content: 'RevX 是票券平台，帶您開啟探索新時代的入口'
+        }
+    ]
+    return (
+        <section className="slider-container">
+            <ul>
+                {
+                    history.filter((item) => item.id === current).map((item) => {
+                        console.log(400 * item.id / history.length);
+                        return (
+                            <li key={item.id}>
+                                <section>
+                                    <div className="timeline">
+                                        <div className="star"
+                                            style={{ top: `${400 * item.id / history.length}px` }}>
+                                            <ReactSVG src={process.env.PUBLIC_URL + '/img/star.svg'} />
+                                        </div>
+
+                                        <div className="vertical-line"></div>
+                                    </div>
+                                    <div className="main">
+                                        <h1>{item.date}</h1>
+                                        <h2>{item.title}</h2>
+                                        <p>{item.content}</p>
+                                    </div>
+                                    <div className="img-area">
+                                        <img className="blockchain" src={process.env.PUBLIC_URL + '/img/blockchain-1.png'} alt='block chain' />
+                                        <img className="man" src={process.env.PUBLIC_URL + '/img/man-1.png'} alt='man' />
+                                    </div>
+                                </section>
+                                <div className="prev-next">
+                                    {item.id > 0 && <div className="arrow-left" onClick={() => setCurrent(n => n - 1)}><div></div></div>}
+                                    {item.id < history.length - 1 && <div div className="arrow-right" onClick={() => setCurrent(n => n + 1)}><div></div></div>}
+                                </div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        </section >
+    )
+
+}
+
+export default Slider;
--- src/pages/Introduction/Introduction.js
+++ src/pages/Introduction/Introduction.js
@@ -4,8 +4,24 @@ import Header from "../../components/Header/Header";
 import Footer from '../../components/Footer/Footer'
 import BannerTitle from "../../components/BannerTitle/BannerTitle";
 import Marquee from "../../components/Marquee/Marquee";
+import Slider from "../../components/Slider/Slider";
 
 const Introduction = () => {
+    const brands = [
+        {
+            title: 'AP 愛完美',
+            img: '/img/brands/ap.png',
+            content: 'AP 愛完美成立於 2020 年，優質選品，讓您感知生活，享受生活。專營國際知名品牌化妝品，保養品，香水等，致力於打造奢華、藝術、完美的購物體驗。\n與國內信譽卓著廠商及代理商成為貿易夥伴，確保我們客戶所購買的商品品質及嚴格把關排除仿冒品。'
+        }, {
+            title: 'RevX',
+            img: '/img/brands/RevX.png',
+            content: 'RevX 擁有獨立的票券後台系統。可以有效地管理票券銷售和分發。另外，您可以根據自己的業務需求選擇最適合的模組，無論是規模大小、預算或特定功能需求，都能找到最適合的解決方案。'
+        }, {
+            title: 'Throne of Dawn',
+            img: '/img/brands/throne-of-dawn.png',
+            content: '背景故事以現在市面少見的奇幻冒險史詩風格為主軸，融合魔法和神話元素，搭配精美的角色設計和細膩唯美的美術視覺，講述在永夜降臨後，四大種族的各個英雄紛紛站出，發起了一系列搶奪王座之戰。'
+        }
+    ]
     return (
         <div className="home-wrapper">
             <Header />
@@ -19,15 +35,58 @@ const Introduction = () => {
                         Actions speak louder than words. Persistence propels triumph. Adversity forges discipline. Manners maketh man.  More than a brand— APAM embodies a dynamic spirit, unwavering commitment, and enduring impact.
                     </p>
                     <div className="social-media">
-                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/fb.svg'} />
-                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/instagram.svg'} />
-                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/line.svg'} />
-                    <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/twitter.svg'} />
+                        <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/fb.svg'} />
+                        <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/instagram.svg'} />
+                        <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/line.svg'} />
+                        <ReactSVG src={process.env.PUBLIC_URL + '/img/icon/social_media/twitter.svg'} />
+                    </div>
                 </div>
-                </div>
+                <Marquee />
                 <div className="brand-essence">
-                <Marquee/>
+
+                    <p>
+                        <span className="slogan">行動</span>
+                        <span className="slogan">毅力</span>
+                        <br />
+                        <span className="slogan">挑戰</span>
+                        <span className="slogan">禮貌</span>
+                    </p>
+                    <article>
+                        <p>行動勝過言辭，毅力助力成功，逆境培養紀律，禮儀雕塑品格。不僅僅是品牌，更是一種充滿活力的精神——對堅定承諾和持久影響的明證。感受本質，擁抱旅程。</p>
+                        <p>公司成立於 2021 年，我們專注於軟體開發服務，團隊主要均有相關資深背景。從 Web 2.0 向新時代邁進。
+                            <br />
+                            我們的技術和網絡安全專家團隊提供多種解決方案，致力助企業透過創新技術進一步發展業務。</p>
+                    </article>
                 </div>
+
+                <Slider />
+
+                <section className="brands">
+                    <ul>
+                        {
+                            brands.map((item, index) => {
+                                return (
+                                    <li key={index}>
+                                        <div className="img-area-out">
+                                            <div className="img-area-in">
+                                                <img src={process.env.PUBLIC_URL + item.img} alt={item.title} />
+                                            </div>
+                                        </div>
+                                        <article>
+                                            <h3>{item.title}</h3>
+                                            <p>{item.content}</p>
+                                        </article>
+
+                                        <div className="more">
+                                            <span>MORE</span>
+                                            <ReactSVG src={process.env.PUBLIC_URL + '/img/arrow.svg'} />
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </section>
 
             </div>
             <Footer />
